3438fcc02f102eeac08ea0c0360945d6932d0c72
siddupatil123 <116625505+siddupatil123@users.noreply.github.com>
2022-11-26 10:35:02 +0530

    Add files via upload

---
+++ ASSIGNMENT13.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'spreadsheets')
+
+
+# In[ ]:
+
+
+ANSWER:csv file stores the data in comma separated form whereas excel format stores the data in tabular form 
+    excel stores the data in more complex form
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'objects')
+
+
+# In[ ]:
+
+
+ANSWER:we pass csv.read()to read the file whereas csv.write()to write the file
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'in')
+
+ANSWER:first open the file into read mode then we can read the file
+when we open the file into write mode then we can write into file
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'file')
+
+
+# In[ ]:
+
+
+ANSWER: writerow() method helps to make list of arguments into csv file
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'do')
+
+
+# In[ ]:
+
+
+ANSWER:it helps to print the data line by line 
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'structure')
+
+
+# In[ ]:
+
+
+ANSWER:loads()method helps 
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pinfo', 'data')
+
+
+# In[ ]:
+
+
+ANSWER:json.dumps()
+
